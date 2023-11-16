269bc553fc1c0a3ba824dd7d906e5ba85a3904f5
Karolina Widz <karolinawidz97@gmail.com>
2023-11-16 23:33:57 +0100

    Changes in add event layout

--- app/src/main/java/edu/kwjw/you/presentation/AddEventDialogFragment.kt
+++ app/src/main/java/edu/kwjw/you/presentation/AddEventDialogFragment.kt
@@ -5,8 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.datepicker.MaterialDatePicker
 import dagger.hilt.android.AndroidEntryPoint
 import edu.kwjw.you.R
 import edu.kwjw.you.data.remote.dto.AddEventDto
@@ -15,10 +16,16 @@ import edu.kwjw.you.presentation.viewModel.AddEventDialogViewModel
 import java.time.LocalDateTime
 
 @AndroidEntryPoint
-class AddEventDialogFragment : BottomSheetDialogFragment() {
+class AddEventDialogFragment : DialogFragment() {
     private var _binding: FragmentAddEventDialogBinding? = null
     private val binding get() = _binding!!
     private val viewModel: AddEventDialogViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.Theme_App_FullScreenDialog)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,12 +37,35 @@ class AddEventDialogFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentAddEventDialogBinding.bind(view)
-        binding.addButton.setOnClickListener {
+        configureDialog()
+    }
+
+    private fun configureDialog() {
+        binding.toolbar.setNavigationOnClickListener { dismiss() }
+        binding.toolbar.setTitle(R.string.add_new_event)
+        binding.toolbar.inflateMenu(R.menu.toolbar_menu)
+        binding.toolbar.setOnMenuItemClickListener {
             viewModel.addNewEvent(
                 AddEventDto(
                     1, "test event", LocalDateTime.now()
                 )
             )
+            dismiss()
+            true
         }
+        binding.eventDateEditText.setOnClickListener { configureDatePicker() }
+    }
+
+    private fun configureDatePicker() {
+        val picker = MaterialDatePicker.Builder
+            .datePicker()
+            .setInputMode(MaterialDatePicker.INPUT_MODE_CALENDAR)
+            .build()
+
+        picker.show(parentFragmentManager, DATE_PICKER_TAG)
+    }
+
+    companion object {
+        const val DATE_PICKER_TAG = "DatePicker"
     }
 }
