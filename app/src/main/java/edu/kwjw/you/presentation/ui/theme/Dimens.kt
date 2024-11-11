035f43768c04e41f4b828589402dfa4a0696f3c3
Karolina Widz <karolinawidz97@gmail.com>
2024-11-11 20:26:37 +0100

    Add fonts, dimens, type and themes

---
+++ app/src/main/java/edu/kwjw/you/presentation/ui/theme/Dimens.kt
@@ -0,0 +1,14 @@
+package edu.kwjw.you.presentation.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val PaddingExtraSmall = 2.dp
+val PaddingSmall = 4.dp
+val PaddingMedium = 8.dp
+val PaddingLarge = 16.dp
+val PaddingExtraLarge = 32.dp
+
+val SizeMedium = 56.dp
+
+val CornerRadiusSmall = 4.dp
+val CornerRadiusMedium = 8.dp
