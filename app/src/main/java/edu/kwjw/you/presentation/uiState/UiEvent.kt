d6dcd53bad7f48b361ce83280f573ceb3440883c
Karolina Widz <karolinawidz97@gmail.com>
2024-12-18 16:38:46 +0100

    Switch list screen to compose

--- app/src/main/java/edu/kwjw/you/presentation/uiState/UiEvent.kt
+++ app/src/main/java/edu/kwjw/you/presentation/uiState/UiEvent.kt
@@ -1,13 +1,15 @@
 package edu.kwjw.you.presentation.uiState
 
-import edu.kwjw.you.domain.model.Event
+import edu.kwjw.you.presentation.ui.eventlist.EventItem
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 sealed class UiEvent {
     data class ShowDialogSnackbar(val message: String) : UiEvent()
     data class ShowListSnackbar(val message: String) : UiEvent()
     object DismissDialog : UiEvent()
     data class EventListUpdate(
-        val data: List<Event> = emptyList(),
+        val data: ImmutableList<EventItem> = persistentListOf(),
         val state: EventState = EventState.LOADING
     ) : UiEvent() {
         enum class EventState { LOADING, SUCCESS, ERROR }
