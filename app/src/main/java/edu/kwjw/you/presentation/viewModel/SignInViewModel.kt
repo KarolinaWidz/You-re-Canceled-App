d6fb8a48dbdae475e35161d9eca9007172a76e92
Karolina Widz <karolinawidz97@gmail.com>
2025-04-22 21:47:50 +0200

    Add signing in

--- app/src/main/java/edu/kwjw/you/presentation/viewModel/SignInViewModel.kt
+++ app/src/main/java/edu/kwjw/you/presentation/viewModel/SignInViewModel.kt
@@ -1,21 +1,29 @@
 package edu.kwjw.you.presentation.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.kwjw.you.data.repository.UserAccountRepository
 import edu.kwjw.you.presentation.uiState.SignInIntent
 import edu.kwjw.you.presentation.uiState.SignInState
+import edu.kwjw.you.presentation.uiState.SignInUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@HiltViewModel
+class SignInViewModel @Inject constructor(
+    private val userAccountRepository: UserAccountRepository
+) : ViewModel() {
 
-class SignInViewModel : ViewModel() {
-
-    private val _state = MutableStateFlow(SignInState())
+    private val _state = MutableStateFlow(SignInState(uiState = SignInUiState.Loading))
     val state: StateFlow<SignInState> = _state
 
     fun processIntent(intent: SignInIntent) {
         when (intent) {
-            SignInIntent.SignIn -> TODO()
+            SignInIntent.SignIn -> signIn()
             is SignInIntent.UpdateLogin -> updateLogin(login = intent.login)
             is SignInIntent.UpdatePassword -> updatePassword(password = intent.password)
         }
@@ -24,7 +32,7 @@ class SignInViewModel : ViewModel() {
     private fun updateLogin(login: String) {
         _state.update { state ->
             state.copy(
-                login = login
+                email = login
             )
         }
     }
@@ -34,6 +42,20 @@ class SignInViewModel : ViewModel() {
             state.copy(
                 password = password
             )
+        }
+    }
+
+    private fun signIn() {
+        viewModelScope.launch {
+            _state.update { state -> state.copy(uiState = SignInUiState.Loading) }
+            userAccountRepository.signInWithEmail(
+                state.value.email,
+                state.value.password
+            ).onSuccess {
+                _state.update { state -> state.copy(uiState = SignInUiState.Success) }
+            }.onFailure {
+                _state.update { state -> state.copy(uiState = SignInUiState.Error) }
+            }
         }
     }
 }
