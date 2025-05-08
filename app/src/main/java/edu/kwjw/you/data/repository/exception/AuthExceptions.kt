6a6529313b91a40161977d4ba39bbce4f8e5fd54
Karolina Widz <karolinawidz97@gmail.com>
2025-05-08 17:26:32 +0200

    Add encrypted data store

--- app/src/main/java/edu/kwjw/you/data/repository/exception/AuthExceptions.kt
+++ app/src/main/java/edu/kwjw/you/data/repository/exception/AuthExceptions.kt
@@ -1,4 +1,4 @@
-package edu.kwjw.you.domain.exception
+package edu.kwjw.you.data.repository.exception
 
 class UserNotFoundException : Exception("User has not been found")
 class TokenRetrievalException : Exception("Could not retrieve the token")
